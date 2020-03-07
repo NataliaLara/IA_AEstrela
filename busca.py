85198d210cf68359e1d1016a4665deeb2299539c
natalia <naty.lara@hotmail.com>
2020-03-07 11:38:16 -0300

    first commit

--- busca.py
+++ busca.py
@@ -3,17 +3,17 @@ import os
 path = os.getcwd()
 
 distToBuch = pd.read_csv('distanceToBucharest.csv', delimiter=';')
-aCityToB = pd.read_csv('AcityToBcityDistance.csv')
+aCityToB = pd.read_csv('AcityToBcityDistance.csv',delimiter=';')
 
-print(distToBuch['City'])
+#print(distToBuch['City']) 
 #print(distToBuch['DistanceToBucharest'])
 #for row in distToBuch:
 	#print(row)
 
-#for row,index in distToBuch.iterrows():
-#	print(row)
-
 for index, row in distToBuch.iterrows():
     print (row['City'], row['DistanceToBucharest'])
+
+for index, row in aCityToB.iterrows():
+    print (row['OriginCity'], row['FinalCity'],row['Distance'])
  
 
